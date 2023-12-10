568d6469e04a2fc0c22c394074fec221934629f9
Polina Simonova <polina.simonova@mail.ru>
2023-12-10 14:30:45 +0300

    Сделано ДЗ по рекурсиям

---
+++ ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+
+int Akkerman(int m, int n) {
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if(m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+int a = Akkerman(0,1);
+System.Console.WriteLine($"a = {a}");
+
+int b = Akkerman(2,0);
+System.Console.WriteLine($"b = {b}");
+
+int c = Akkerman(2,3);
+System.Console.WriteLine($"c = {c}");
+
+int d = Akkerman(3,2);
+System.Console.WriteLine($"d = {d}");
+
---
+++ ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] array = [1,2,3,4,5,6,4,2,4,5,2,1,4,5,8];
+
+
+void Recursion(int[] arr, int index) {
+    System.Console.WriteLine(arr[index]); 
+    if (index != 0){
+        Recursion(arr, index - 1);
+    }
+}
+
+
+Recursion(array, array.Length - 1);
